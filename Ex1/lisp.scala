ed2fd5d1a44311e6cacb447a42206469eb1bb801
Al153 <al.taylor1105@gmail.com>
2018-10-16 21:15:12 +0100

    Step 1 complete

--- Ex1/lisp.scala
+++ Ex1/lisp.scala
@@ -1,25 +1,42 @@
 package lisp
 
 object ast {
+
   trait Value
+
   case object Undefined extends Value
-  case class B(b: Boolean) extends Value                      // Boolean
-  case class I(n: Int) extends Value                          // Int
-  case class S(sym: String) extends Value                     // Symbol
-  case object N extends Value                                 // Nil
-  class P(var car: Value, var cdr: Value) extends Value       // Pair
+
+  case class B(b: Boolean) extends Value // Boolean
+  case class I(n: Int) extends Value // Int
+  case class S(sym: String) extends Value // Symbol
+  case object N extends Value // Nil
+  class P(var car: Value, var cdr: Value) extends Value // Pair
   {
     override def toString = s"P($car, $cdr)"
+
     // NOTE: we don't override equals to respect referential equality
   }
+
   object P {
     def apply(a: Value, b: Value): P = new P(a, b)
+
     def unapply(v: Value): Option[(Value, Value)] = v match {
-      case p:P => Some((p.car, p.cdr))
+      case p: P => Some((p.car, p.cdr))
       case _ => None
     }
   }
-  case class F(f: Value => Value) extends Value               // Procedures
+
+  object EvalArgs {
+    def apply(expr: Value, env: Env, cont: Cont): P = P(expr, P(env, P(cont, N)))
+
+    def unapply(arg: Value): Option[(Value, Env, Cont)] = arg match {
+      case P(expr, P(env: Env, P(cont: Cont, N))) => Some((expr, env, cont))
+      case _ => None
+    }
+  }
+
+  case class F(f: Value => Value) extends Value // Procedures
+  case class Fsubr(f: Value => Value) extends Value
 
   // Env is a list of frames (each a list of key/value pairs)
   // We use object structures for easy reification/reflection.
@@ -31,13 +48,15 @@ object ast {
     case N => Nil
     case P(first, rest) => first :: list(rest)
   }
+
   def valueOf(es: List[Value]): Value = es match {
     case Nil => N
-    case first::rest => P(first, valueOf(rest))
+    case first :: rest => P(first, valueOf(rest))
   }
 }
 
-import ast._
+import lisp.ast._
+
 object eval {
   //def base_eval(exp: Value, env: Env, cont: Cont): Value = {
   def base_eval(exp: Value, env: Env, cont: Cont): Value = debug(s"eval ${pp.show(exp)}", env, cont) { (cont) =>
@@ -63,84 +82,110 @@ object eval {
   }
 
   def eval_if(exp: Value, env: Env, cont: Cont): Value = exp match {
-    case P(_, P(c, P(a, P(b, N)))) => base_eval(c, env, F{ cv => cv match {
-      case B(false) => base_eval(b, env, cont)
-      case B(true) => base_eval(a, env, cont)
-    }})
+    case P(_, P(c, P(a, P(b, N)))) => base_eval(c, env, F { cv =>
+      cv match {
+        case B(false) => base_eval(b, env, cont)
+        case B(true) => base_eval(a, env, cont)
+      }
+    })
   }
 
   def eval_set_bang(exp: Value, env: Env, cont: Cont): Value = exp match {
-    case P(_, P(S(x), P(rhs, N))) => base_eval(rhs, env, F{ v =>
+    case P(_, P(S(x), P(rhs, N))) => base_eval(rhs, env, F { v =>
       cont.f(set(env, x, v))
     })
   }
 
   def eval_lambda(exp: Value, env: Env, cont: Cont): Value = exp match {
-    case P(_, P(params, body)) => cont.f(F({args =>
-      eval_begin(body, extend(env, params, args), F{v => v})
+    case P(_, P(params, body)) => cont.f(F({ args =>
+      eval_begin(body, extend(env, params, args), F { v => v })
     }))
   }
 
   def eval_begin(exp: Value, env: Env, cont: Cont): Value = exp match {
     case P(e, N) => base_eval(e, env, cont)
-    case P(e, es) => base_eval(e, env, F{ _ => eval_begin(es, env, cont) })
+    case P(e, es) => base_eval(e, env, F { _ => eval_begin(es, env, cont) })
   }
 
   def eval_define(exp: Value, env: Env, cont: Cont): Value = exp match {
     case P(_, P(r@S(name), body)) => {
-      val p = P(r,Undefined)
+      val p = P(r, Undefined)
       env.car = P(p, env.car)
-      eval_begin(body, env, F{v =>
+      eval_begin(body, env, F { v =>
         p.cdr = v
-        cont.f(r)})
+        cont.f(r)
+      })
     }
   }
 
   def eval_application(exp: Value, env: Env, cont: Cont): Value = exp match {
-    case P(fun, args) => base_eval(fun, env, F{ vf => vf match {
-      case F(f) => evlist(args, env, F{ vas => cont.f(f(vas)) })
-    }})
+    case P(fun, args) => base_eval(fun, env, F {
+      case F(f) => evlist(args, env, F { vas => cont.f(f(vas)) })
+      /*case Fsubr(f) => f(EvalArgs(exp, env, cont)) */
+    })
   }
 
   def evlist(exp: Value, env: Env, cont: Cont): Value = exp match {
     case N => cont.f(N)
-    case P(first, rest) => base_eval(first, env, F{v => evlist(rest, env, F{vs => cont.f(P(v,vs))})})
+    case P(first, rest) => base_eval(first, env, F { v => evlist(rest, env, F { vs => cont.f(P(v, vs)) }) })
   }
 
   def extend(env: Env, params: Value, args: Value): Env = {
-    val frame = valueOf((list(params) zip  list(args)).map{t => P(t._1, t._2)})
+    val frame = valueOf((list(params) zip list(args)).map { t => P(t._1, t._2) })
     P(frame, env)
   }
 
   def findFrame(frame: Value, x: String): Option[P] = frame match {
     case N => None
-    case P(P(S(y),_), _) if (x==y) => Some(frame.asInstanceOf[P].car.asInstanceOf[P])
+    case P(P(S(y), _), _) if x == y => Some(frame.asInstanceOf[P].car.asInstanceOf[P])
     case P(_, rest) => findFrame(rest, x)
   }
+
   def find(env: Env, x: String): P = env match {
-    case P(first,rest) => findFrame(first, x) match {
+    case P(first, rest) => findFrame(first, x) match {
       case Some(p) => p
       case None => rest match {
-        case next:Env => find(next, x)
+        case next: Env => find(next, x)
         case _ => sys.error(s"unbound variable $x")
       }
     }
   }
+
   def get(env: Env, x: String): Value = find(env, x).cdr
+
   def set(env: Env, x: String, v: Value): Value = {
     val p = find(env, x)
     p.cdr = v
     v
   }
 
+  def toIntList(value: ast.Value): List[Int] =
+    value match {
+      case N => Nil
+      case P(I(a), rest) => a :: toIntList(rest)
+    }
+
   lazy val init_env: Env = P(valueOf(List(
-    P(S("eq?"), F({args => args match { case P(a, P(b, N)) => B(a==b) }})))), N)
+    P(S("eq?"), F({ case P(a, P(b, N)) => B(a == b) })),
+    P(S("<"), F({ case P(I(a), P(I(b), N)) => B(a < b) })),
+    P(S("*"), F({ args => I(toIntList(args).product) })),
+    P(S("-"), F({ case P(I(a), P(I(b), N)) => I(a - b) })),
+
+    /*P(S("quote"), F({ case EvalArgs(value, env, cont) => eval_quote(value, env, cont)})),
+    P(S("if"), F({ case EvalArgs(value, env, cont) => eval_if(value, env, cont)})),
+
+    P(S("set!"), F({ case EvalArgs(value, env, cont) => eval_set_bang(value, env, cont)})),
+    P(S("lambda"), F({ case EvalArgs(value, env, cont) => eval_lambda(value, env, cont)})),
+    P(S("begin"), F({ case EvalArgs(value, env, cont) => eval_begin(value, env, cont)})),
+    P(S("define"), F({ case EvalArgs(value, env, cont) => eval_define(value, env, cont)})) */
+  )), N)
 }
 
 import scala.util.parsing.combinator._
+
 object parser extends JavaTokenParsers with PackratParsers {
-    def exp: Parser[Value] =
-      "#f" ^^ { case _ => B(false) } |
+  def exp: Parser[Value] =
+    "#f" ^^ { case _ => B(false) } |
       "#t" ^^ { case _ => B(true) } |
       wholeNumber ^^ { case s => I(s.toInt) } |
       """[^\s\(\)'"]+""".r ^^ { case s => S(s) } |
@@ -149,20 +194,27 @@ object parser extends JavaTokenParsers with PackratParsers {
       "(" ~> exps <~ ")" ^^ { case vs => vs }
 
   def exps: Parser[Value] =
-      exp ~ exps ^^ { case v~vs => P(v, vs) } |
+    exp ~ exps ^^ { case v ~ vs => P(v, vs) } |
       exp ^^ { case v => P(v, N) }
 }
 
-import eval._
-import parser._
+import lisp.eval._
+import lisp.parser._
+
 object repl {
   var global_env = init_env
+
   def parse(s: String) = {
     val Success(e, _) = parseAll(exp, s)
     e
   }
-  def evl(e: Value) = { base_eval(e, global_env, F{ v => v } ) }
+
+  def evl(e: Value) = {
+    base_eval(e, global_env, F { v => v })
+  }
+
   def ev(s: String) = evl(parse(s))
+
   def clean() = {
     global_env = init_env
   }
@@ -171,8 +223,9 @@ object repl {
 object pp {
   def addParen(p: (Boolean, String)) = {
     val (need_paren, s) = p
-    if (need_paren) "("+s+")" else s
+    if (need_paren) "(" + s + ")" else s
   }
+
   def pp(v: Value): (Boolean, String) = v match {
     case B(b) => (false, if (b) "#t" else "#f")
     case I(n) => (false, n.toString)
@@ -182,20 +235,27 @@ object pp {
     case P(a, d) =>
       val s1 = addParen(pp(a))
       val (need_paren2, s2) = pp(d)
-      if (need_paren2) (true, s1+" "+s2)
-      else (true, s1+" . "+s2)
+      if (need_paren2) (true, s1 + " " + s2)
+      else (true, s1 + " . " + s2)
     case _ => (false, v.toString)
   }
+
   def show(v: Value) = addParen(pp(v))
+
   def display(v: Value) = print(show(v))
+
   def newline() = println("")
 }
 
-import repl._
-import pp._
+import lisp.pp._
+import lisp.repl._
 import utils._
-class lisp_Tests extends TestSuite {  before { clean() }
-  ignore("(factorial 6)") {
+
+class lisp_Tests extends TestSuite {
+  before {
+    clean()
+  }
+  test("(factorial 6)") {
     ev("""(define factorial (lambda (n) (if (< n 2) n (* n (factorial (- n 1))))))""")
     assertResult(I(720))(ev("(factorial 6)"))
   }
@@ -207,7 +267,8 @@ class lisp_Tests extends TestSuite {  before { clean() }
   }
 
   ignore("(odd 7)") {
-    ev("""(begin
+    ev(
+      """(begin
 (define even (lambda (n) (if (eq? n 0) #t (odd (- n 1)))))
 (define odd (lambda (n) (if (eq? n 0) #f (even (- n 1)))))
 )""")
@@ -238,11 +299,12 @@ class lisp_Tests extends TestSuite {  before { clean() }
     ev("(define history '())")
     assertResult(N)(ev("history"))
     assertResult(show(P(I(4), N)))(show(ev("(cons 4 history)")))
-   }
+  }
 
   ignore("fexpr history") {
     ev("(define history '())")
-    ev("""(define save! (fexpr (lhs rhs)
+    ev(
+      """(define save! (fexpr (lhs rhs)
    ((lambda (old-val)
      (eval (list 'set! lhs rhs))
      (set! history (cons (list
@@ -259,27 +321,29 @@ class lisp_Tests extends TestSuite {  before { clean() }
   }
 
   ignore("fsubr") {
-     ev("(define my-exp (fsubr (exp env cont) exp))")
-     assertResult("(my-exp x)")(show(ev("(my-exp x)")))
-     ev("(define jump (fsubr (exp env cont) (eval (car (cdr exp)))))")
-     assertResult(I(2))(ev("(- 1 (jump 2))"))
-     ev("(define fall (fsubr (exp env cont) 1))")
-     assertResult(I(1))(ev("(* 2 (fall))"))
-     // NOTE: to work nicely with composing continuations,
-     // we would have to adjust the calling conventions...
-   }
+    ev("(define my-exp (fsubr (exp env cont) exp))")
+    assertResult("(my-exp x)")(show(ev("(my-exp x)")))
+    ev("(define jump (fsubr (exp env cont) (eval (car (cdr exp)))))")
+    assertResult(I(2))(ev("(- 1 (jump 2))"))
+    ev("(define fall (fsubr (exp env cont) 1))")
+    assertResult(I(1))(ev("(* 2 (fall))"))
+    // NOTE: to work nicely with composing continuations,
+    // we would have to adjust the calling conventions...
+  }
 
   ignore("fsubr history") {
     ev("(define old-set! set!)")
     ev("(define history '())")
-    ev("""(define save! (fexpr (lhs rhs)
+    ev(
+      """(define save! (fexpr (lhs rhs)
    ((lambda (old-val)
      (eval (list 'old-set! lhs rhs))
      (old-set! history (cons (list
         lhs
         old-val (eval lhs)) history)))
    (eval lhs))))""")
-    ev("""(set! set! (fsubr (exp env cont)
+    ev(
+      """(set! set! (fsubr (exp env cont)
       (eval (list 'save! (car (cdr exp)) (car (cdr (cdr exp)))))
       (cont (car (cdr exp)))))""")
     ev("(define test 1)")
@@ -292,8 +356,8 @@ class lisp_Tests extends TestSuite {  before { clean() }
   }
 }
 
-import lisp._
-import ast._
+import lisp.ast._
+
 object debug {
   val enable = false
   var depth: Int = 0
@@ -319,16 +383,17 @@ object debug {
     s.split("\n").mkString(if (padFirst) padding else "", "\n" + padding, "")
 
   def indentedDebug(msg: String) =
-    if(enable) println(pad(msg, padFirst = true))
+    if (enable) println(pad(msg, padFirst = true))
 
   implicit class EnvDeco(val env: Env) extends AnyVal {
     def format: String =
       "---------env-------\n" ++
-      list(env).map(formatFrame).mkString("\n") ++
-      "\n---------env-------\n"
+        list(env).map(formatFrame).mkString("\n") ++
+        "\n---------env-------\n"
 
     def formatFrame(frame: Value): String = list(frame).map {
       case P(S(name), body) => name + " -> " + body
     }.mkString("\n")
   }
+
 }
